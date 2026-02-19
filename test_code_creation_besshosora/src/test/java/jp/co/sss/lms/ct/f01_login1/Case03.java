2523033c9e3106ce63c28cb3abe5595c0529c903
sorabessho <sora_bessho@asupage.co.jp>
2026-02-19 15:47:43 +0900

    fix:Case03.java test01(),test02()メソッドに記述追加
    test01()
    トップページURLでアクセスし、検証し、エビデンスを取得する記述を追加
    記述内容はCase01と同様
    
    test02()
    初回ログイン済みの受講生ユーザーでログインし、検証、エビデンスを取得する記述を追加
    WebElementを利用し、画面操作。
    画面遷移後、WebElement、assertEqualsを利用し検証
    期待値は以下の通り。
    -画面タイトル：コース詳細
    検証後、エビデンスを取得。

--- test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -31,18 +34,51 @@ public class Case03 {
 		closeDriver();
 	}
 
+	/**
+	 * @author 別所
+	 */
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//URLに遷移
+		goTo("http://localhost:8080/lms/");
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
+	/**
+	 * @author 別所
+	 */
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面操作
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser1234");
+
+		WebElement loginBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']"));
+		loginBtnElement.click();
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.cssSelector("li.active"));
+		assertEquals("コース詳細", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
