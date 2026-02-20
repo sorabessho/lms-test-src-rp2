0f507321afee9d6c82d16577f5f25588f2b36347
sorabessho <sora_bessho@asupage.co.jp>
2026-02-20 11:02:33 +0900

    fix:Case05.java テストコードの作成
    test01()
    トップページURLでアクセス
    記述内容はCase04.test01()と同様
    
    test02()
    初回ログイン済みの受講生ユーザーでログイン
    記述内容はCase04.test02()と同様
    
    test03()
    上部メニューの「ヘルプ」リンクからヘルプ画面に遷移
    記述内容はCase04.test03()と同様
    
    test04()
    「よくある質問」リンクからよくある質問画面を別タブに開く
    記述内容はCase04.test04()と同様
    
    test05()
    キーワード検索で該当キーワードを含む検索結果だけ表示
    WebElement、webDriver.findElementを利用して、キーワードに値を入力し、検索。
    WebElement、webDriver.findElement、assertEquals()を利用して検索結果を検証。
    期待値は以下の通り
    -Q.助成金書類の作成方法が分かりません
    その後、検索結果が画面に出るようにスクロールし、エビデンスを取得。
    
    test06()
    「クリア」ボタン押下で入力したキーワードを消去
    WebElement、webDriver.findElementを利用して、クリアボタンを押下。
    WebElement、webDriver.findElement、assertEquals()を利用して「キーワード」テキストボックス検証。
    期待値は以下の通り
    -空文字
    その後、エビデンスを取得。

--- test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
+++ test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f02_faq/Case05.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -38,41 +38,127 @@ public class Case05 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//URLに遷移
+		goTo("http://localhost:8080/lms/");
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面操作
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser1234");
+
+		WebElement loginBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']"));
+		loginBtnElement.click();
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.cssSelector("li.active"));
+		assertEquals("コース詳細", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
-	
+
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//画面操作
+		WebElement dropDownElement = webDriver.findElement(By.cssSelector("li.dropdown"));
+		dropDownElement.click();
+
+		WebElement linkElement = webDriver.findElement(By.cssSelector("a[href='/lms/help']"));
+		linkElement.click();
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ヘルプ", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		//画面操作
+		WebElement linkElement = webDriver.findElement(By.cssSelector("a[href='/lms/faq']"));
+		linkElement.click();
+
+		//開いたタブに移動
+		String currentTab = webDriver.getWindowHandle();
+		System.out.println(currentTab);
+		for (String newTab : webDriver.getWindowHandles()) {
+			if (!newTab.equals(currentTab)) {
+				webDriver.switchTo().window(newTab);
+				break;
+			}
+		}
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("よくある質問", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
+
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 キーワード検索で該当キーワードを含む検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+		//画面操作
+		WebElement keywordElement = webDriver.findElement(By.id("form"));
+		keywordElement.clear();
+		keywordElement.sendKeys("助成金");
+
+		WebElement searchBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='検索']"));
+		searchBtnElement.click();
+
+		//検索結果検証
+		WebElement searchResultElement = webDriver.findElement(By.cssSelector("dt.mb10"));
+		assertEquals("Q.助成金書類の作成方法が分かりません", searchResultElement.getText());
+
+		//エビデンス取得
+		scrollBy("800");
+		getEvidence(new Object() {
+		});
 	}
-	
+
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 「クリア」ボタン押下で入力したキーワードを消去")
 	void test06() {
-		// TODO ここに追加
+		//画面操作
+		WebElement searchBtnElement = webDriver.findElement(By.cssSelector("input[type='button'][value='クリア']"));
+		searchBtnElement.click();
+
+		//「キーワード」テキストボックス検証
+		WebElement keywordElement = webDriver.findElement(By.id("form"));
+		assertEquals("", keywordElement.getAttribute("value"));
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
