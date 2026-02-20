da2bac8e4b1fa61b46c1efe7291be037b23153f1
sorabessho <sora_bessho@asupage.co.jp>
2026-02-20 09:17:52 +0900

    fix:Case04.java テストコードの作成
    test01()
    トップページにアクセス
    処理内容は
    Case03.test01()と同様
    
    test02()
    初回ログイン済みの受講生ユーザでログイン
    処理内容は
    Case03.test01()と同様
    
    test03()
    上部メニューの「ヘルプ」リンクからヘルプ画面に遷移
    WebElement、findElement()を利用してヘッダーのプルダウンメニューを開き、「ヘルプ」リンクをクリックし画面遷移。
    画面遷移後、assertEquals()で画面タイトルを検証。
    その後、エビデンスを取得。
    
    test04()
    「よくある質問」リンクからよくある質問画面を別タブに開く
    WebElement、findElement()を利用して「よくある質問」リンクをクリック。
    クリックすると、別タブが開かれるので、webDriverのgetWindowHandle()、getWindowHandles()、switchTo().window(新しいタブ)を使用して、別タブに移動。
    その処理の後に、assertEquals()で画面タイトルを検証。
    その後、エビデンス取得。

--- test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,28 +38,88 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//URLに遷移
+		goTo("http://localhost:8080/lms/");
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面操作
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser1234");
+
+		WebElement loginBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']"));
+		loginBtnElement.click();
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.cssSelector("li.active"));
+		assertEquals("コース詳細", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//画面操作
+		WebElement dropDownElement = webDriver.findElement(By.cssSelector("li.dropdown"));
+		dropDownElement.click();
+
+		WebElement linkElement = webDriver.findElement(By.cssSelector("a[href='/lms/help']"));
+		linkElement.click();
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ヘルプ", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		//画面操作
+		WebElement linkElement = webDriver.findElement(By.cssSelector("a[href='/lms/faq']"));
+		linkElement.click();
+
+		//開いたタブに移動
+		String currentTab = webDriver.getWindowHandle();
+		System.out.println(currentTab);
+		for (String newTab : webDriver.getWindowHandles()) {
+			if (!newTab.equals(currentTab)) {
+				webDriver.switchTo().window(newTab);
+				break;
+			}
+		}
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("よくある質問", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
