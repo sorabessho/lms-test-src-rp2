1f9468323cb2bcb39ab8c48ebdf9d775cd34cb14
sorabessho <sora_bessho@asupage.co.jp>
2026-02-20 16:20:50 +0900

    fix:Case07.java テストコードの作成
    test01()
    トップページURLでアクセス
    記述内容はCase06.test01()と同様
    
    test02()
    初回ログイン済みの受講生ユーザーでログイン
    記述内容はCase06.test02()と同様
    
    test03()
    未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移
    WebElement、webDriver.findElements()、webDriver.findElement()を利用して、未提出の日の「詳細」ボタンを押下。
    まずは、全ての日付が入った講義内容ごとのブロックをList型で取得。
    それをfor文を二つ、if文を一つ使用し、未提出の日を探す。
    見つけたら、.executeScript、.scrollIntoViewで見つけた要素までスクロール。
    その後、ボタンを押下。
    見つけた時点で全てのfor文を終了。
    画面遷移後、画面タイトルを検証。
    期待値は下記通り
    -セクション詳細
    その後エビデンス取得。
    
    test04()
    「提出する」ボタンを押下しレポート登録画面に遷移
    WebElement、webDriver.findElement()を利用して、「日報を提出する」ボタンを押下。
    画面遷移後、legendタグを利用し、タイトル検証。
    その後エビデンス取得。
    
    test05()
    報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される
    WebElement、webDriver.findElement()を利用して、テキストボックスに値を入力し、「提出」ボタンを入力する。
    その後、ボタンの内容が変更されているか検証。
    期待値は下記通り
    -提出済み日報【デモ】を確認する
    その後、エビデンス取得。

--- test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
+++ test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f03_report/Case07.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f03_report;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト レポート機能
@@ -35,35 +41,112 @@ public class Case07 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//URLに遷移
+		goTo("http://localhost:8080/lms/");
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面操作
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser1234");
+
+		WebElement loginBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']"));
+		loginBtnElement.click();
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.cssSelector("li.active"));
+		assertEquals("コース詳細", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 未提出の研修日の「詳細」ボタンを押下しセクション詳細画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//画面操作
+		List<WebElement> lectureCategoryElements = webDriver.findElements(By.cssSelector("div.panel-body"));
+
+		outer: for (WebElement lectureCategoryElement : lectureCategoryElements) {
+			List<WebElement> sessionElements = lectureCategoryElement.findElements(By.tagName("tr"));
+			for (WebElement sessionElement : sessionElements) {
+				boolean isSubmit = sessionElement.findElement(By.cssSelector("td.w10per")).getText().equals("未提出");
+				if (isSubmit) {
+					WebElement btnElement = sessionElement
+							.findElement(By.cssSelector("input[type='submit'][value='詳細']"));
+					((JavascriptExecutor) webDriver).executeScript(
+							"arguments[0].scrollIntoView({block:'center', inline:'nearest'});",
+							btnElement);
+					btnElement.click();
+					break outer;
+				}
+			}
+		}
+
+		//画面タイトル検証
+		WebElement titleElement = webDriver.findElement(By.cssSelector("li.active"));
+		assertEquals("セクション詳細", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
+
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「提出する」ボタンを押下しレポート登録画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//画面操作
+		WebElement dailyReportBtn = webDriver.findElement(By.cssSelector("input[type='submit']"));
+		dailyReportBtn.click();
+
+		//タイトル検証
+		WebElement titleElement = webDriver.findElement(By.tagName("legend"));
+		assertEquals("報告レポート", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 報告内容を入力して「提出する」ボタンを押下し確認ボタン名が更新される")
 	void test05() {
-		// TODO ここに追加
+		//画面操作
+		WebElement textareaElement = webDriver.findElement(By.tagName("textarea"));
+		textareaElement.clear();
+		textareaElement.sendKeys("レポート新規登録(日報) 正常系のテストを行います。");
+
+		WebElement submitBtnElement = webDriver.findElement(By.cssSelector("button[type='submit']"));
+		submitBtnElement.click();
+
+		//ボタンが変更されているか検証
+		WebElement dailyReportBtn = webDriver.findElement(By.cssSelector("input[type='submit']"));
+		assertEquals("提出済み日報【デモ】を確認する", dailyReportBtn.getAttribute("value"));
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 }
