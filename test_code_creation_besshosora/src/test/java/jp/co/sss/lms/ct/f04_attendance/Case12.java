3e174293f04b6c21c5cf9d406f78820d58bf3e2e
sorabessho <sora_bessho@asupage.co.jp>
2026-02-26 17:23:37 +0900

    fix:Case12.java テストコードの作成
    checkErrorMessage(String expectedMessage)
    勤怠情報直接編集画面でのエラーメッセージの検証するメソッド。
    
    test01()
    トップページURLでアクセス
    記述内容はCase11.test01()と同様。
    
    test02()
    初回ログイン済みの受講生ユーザーでログイン
    記述内容はCase11.test02()と同様。
    
    test03()
    上部メニューの「勤怠」リンクから勤怠管理画面に遷移
    記述内容はCase11.test03()と同様。
    
    test04()
    「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移
    記述内容はCase11.test04()と同様。
    
    test05()
    不適切な内容で修正してエラー表示：出退勤の（時）と（分）のいずれかが空白
    Selectクラスを使い、「退勤（分）」要素を取得し、操作。
    Util.clickUpdateBtn()を利用し、「更新」ボタンを押下。
    Util.acceptJSAlert()を利用し、確認モーダルウィンドウを処理。
    Util.checkTitleH2()を利用し、画面タイトルを検証。
    Case12.checkErrorMessage()を利用し、エラーメッセージを検証。
    Util.checkBoxErrorを利用しボックスエラー表示を検証。
    検証がすべて完了後、エビデンスを取得。
    
    test06()
    不適切な内容で修正してエラー表示：出勤が空白で退勤に入力あり
    Selectクラスを使い、「退勤（分）」要素を取得し、test05()で行った操作を元に戻す。
    Selectクラスを使い、「出勤（時間）」・「出勤（分）」要素を取得し、操作。
    更新ボタン押下以降は、test05と同様の処理。
    
    test07()
    Selectクラスを使い、「出勤（時間）」・「出勤（分）」・「退勤（時間）」・「退勤（分）」要素を取得し、操作。
    更新ボタン押下以降は、test05と同様の処理。
    
    test08()
    Selectクラスを使い、「出勤（時間）」・「出勤（分）」・「退勤（時間）」・「退勤（分）」・「中抜け時間」要素を取得し、操作。
    更新ボタン押下以降は、test05と同様の処理。
    
    test09()
    electクラスを使い、「出勤（時間）」・「出勤（分）」・「退勤（時間）」・「退勤（分）」・「中抜け時間」・「備考」要素を取得し、操作。
    更新ボタン押下以降は、test05と同様の処理。

--- test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case12.java
+++ test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case12.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -18,6 +22,18 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース12 受講生 勤怠直接編集 入力チェック")
 public class Case12 {
+
+	/**
+	 * 勤怠情報直接編集画面でのエラーメッセージの検証
+	 * 
+	 * @param expectedMessage 期待するエラーメッセージ
+	 */
+	static void checkErrorMessage(String expectedMessage) {
+		pageLoadTimeout(10);
+		String errorMessageXpath = "//*[@id='main']/div/div/ul/li/span";
+		WebElement errorMessagElement = webDriver.findElement(By.xpath(errorMessageXpath));
+		assertEquals(expectedMessage, errorMessagElement.getText());
+	}
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,63 +51,244 @@ public class Case12 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//URLに遷移
+		goTo("http://localhost:8080/lms/");
+
+		//画面タイトル検証
+		pageLoadTimeout(10);
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面操作
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser1234");
+
+		WebElement loginBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']"));
+		loginBtnElement.click();
+
+		//画面タイトル検証
+		visibilityTimeout(By.cssSelector("li.active"), 10);
+		WebElement titleElement = webDriver.findElement(By.cssSelector("li.active"));
+		assertEquals("コース詳細", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//画面操作
+		WebElement attendanceLinkElement = webDriver.findElement(By.linkText("勤怠"));
+		attendanceLinkElement.click();
+
+		acceptJSAlert();
+
+		//画面タイトル検証
+		checkTitleH2("勤怠管理");
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//画面操作
+		WebElement directEditLinkElement = webDriver.findElement(By.linkText("勤怠情報を直接編集する"));
+		directEditLinkElement.click();
+
+		//画面タイトル検証
+		checkTitleH2("勤怠管理");
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 不適切な内容で修正してエラー表示：出退勤の（時）と（分）のいずれかが空白")
 	void test05() {
-		// TODO ここに追加
+		//画面操作
+		Select endMinuteSelect = new Select(webDriver.findElement(By.id("endMinute1")));
+		endMinuteSelect.selectByIndex(0);
+
+		clickUpdateBtn();
+
+		acceptJSAlert();
+
+		//画面タイトル検証
+		checkTitleH2("勤怠管理");
+
+		//エラーメッセージ検証
+		checkErrorMessage("* 退勤時間が正しく入力されていません。");
+
+		//ボックスエラー表示検証
+		checkBoxError(By.id("endMinute1"));
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(6)
 	@DisplayName("テスト06 不適切な内容で修正してエラー表示：出勤が空白で退勤に入力あり")
 	void test06() {
-		// TODO ここに追加
+		//画面操作
+		Select endMinuteSelect = new Select(webDriver.findElement(By.id("endMinute1")));
+		endMinuteSelect.selectByVisibleText("15");
+
+		Select startHourElement = new Select(webDriver.findElement(By.id("startHour1")));
+		Select startMinuteElement = new Select(webDriver.findElement(By.id("startMinute1")));
+
+		startHourElement.selectByIndex(0);
+		startMinuteElement.selectByIndex(0);
+
+		clickUpdateBtn();
+
+		acceptJSAlert();
+
+		//画面タイトル検証
+		checkTitleH2("勤怠管理");
+
+		//エラーメッセージ検証
+		checkErrorMessage("* 出勤情報がないため退勤情報を入力出来ません。");
+
+		//ボックスエラー表示検証
+		checkBoxError(By.id("startHour1"));
+		checkBoxError(By.id("startMinute1"));
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(7)
 	@DisplayName("テスト07 不適切な内容で修正してエラー表示：出勤が退勤よりも遅い時間")
 	void test07() {
-		// TODO ここに追加
+		//画面操作
+		Select startHourElement = new Select(webDriver.findElement(By.id("startHour1")));
+		Select startMinuteElement = new Select(webDriver.findElement(By.id("startMinute1")));
+		Select endHourSelect = new Select(webDriver.findElement(By.id("endHour1")));
+		Select endMinuteSelect = new Select(webDriver.findElement(By.id("endMinute1")));
+
+		startHourElement.selectByVisibleText("12");
+		startMinuteElement.selectByVisibleText("30");
+		endHourSelect.selectByVisibleText("10");
+		endMinuteSelect.selectByVisibleText("10");
+
+		clickUpdateBtn();
+
+		acceptJSAlert();
+
+		//画面タイトル検証
+		checkTitleH2("勤怠管理");
+
+		//エラーメッセージ検証
+		checkErrorMessage("* 退勤時刻[1]は出勤時刻[1]より後でなければいけません。");
+
+		//ボックスエラー表示検証
+		checkBoxError(By.id("endHour1"));
+		checkBoxError(By.id("endMinute1"));
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(8)
 	@DisplayName("テスト08 不適切な内容で修正してエラー表示：出退勤時間を超える中抜け時間")
 	void test08() {
-		// TODO ここに追加
+		//画面操作
+		Select startHourElement = new Select(webDriver.findElement(By.id("startHour1")));
+		Select startMinuteElement = new Select(webDriver.findElement(By.id("startMinute1")));
+		Select endHourSelect = new Select(webDriver.findElement(By.id("endHour1")));
+		Select endMinuteSelect = new Select(webDriver.findElement(By.id("endMinute1")));
+		Select blankTimeSelect = new Select(
+				webDriver.findElement(By.cssSelector("select[name='attendanceList[1].blankTime']")));
+
+		startHourElement.selectByVisibleText("09");
+		startMinuteElement.selectByVisibleText("00");
+		endHourSelect.selectByVisibleText("12");
+		endMinuteSelect.selectByVisibleText("00");
+		blankTimeSelect.selectByVisibleText("4時間");
+
+		clickUpdateBtn();
+
+		acceptJSAlert();
+
+		//画面タイトル検証
+		checkTitleH2("勤怠管理");
+
+		//エラーメッセージ検証
+		checkErrorMessage("* 中抜け時間が勤務時間を超えています。");
+
+		//ボックスエラー表示検証
+		checkBoxError(By.cssSelector("select[name='attendanceList[1].blankTime']"));
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(9)
 	@DisplayName("テスト09 不適切な内容で修正してエラー表示：備考が100文字超")
 	void test09() {
-		// TODO ここに追加
-	}
+		//画面操作
+		Select startHourElement = new Select(webDriver.findElement(By.id("startHour1")));
+		Select startMinuteElement = new Select(webDriver.findElement(By.id("startMinute1")));
+		Select endHourSelect = new Select(webDriver.findElement(By.id("endHour1")));
+		Select endMinuteSelect = new Select(webDriver.findElement(By.id("endMinute1")));
+		Select blankTimeSelect = new Select(
+				webDriver.findElement(By.cssSelector("select[name='attendanceList[1].blankTime']")));
+		WebElement noteElement = webDriver.findElement(By.cssSelector("input[name='attendanceList[1].note']"));
 
+		startHourElement.selectByVisibleText("08");
+		startMinuteElement.selectByVisibleText("55");
+		endHourSelect.selectByVisibleText("18");
+		endMinuteSelect.selectByVisibleText("15");
+		blankTimeSelect.selectByIndex(0);
+		noteElement.clear();
+		noteElement.sendKeys("あ".repeat(101));
+
+		clickUpdateBtn();
+
+		acceptJSAlert();
+
+		//画面タイトル検証
+		checkTitleH2("勤怠管理");
+
+		//エラーメッセージ検証
+		checkErrorMessage("* 備考の長さが最大値(100)を超えています。");
+
+		//ボックスエラー表示検証
+		checkBoxError(By.cssSelector("input[name='attendanceList[1].note']"));
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
+	}
 }
