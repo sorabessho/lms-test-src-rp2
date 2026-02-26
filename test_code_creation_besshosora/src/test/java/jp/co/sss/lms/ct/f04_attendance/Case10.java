d1c913cfc24ffa22cf2f6a112412dc7ffd4a7b7d
sorabessho <sora_bessho@asupage.co.jp>
2026-02-26 10:05:28 +0900

    fix:Case10.java テストコードの作成
    test01()
    トップページURLでアクセス
    記述内容はCase09.test01()と同様。
    
    test02()
    初回ログイン済みの受講生ユーザーでログイン
    記述内容はCase09.test02()と同様。
    
    test03()
    上部メニューの「勤怠」リンクから勤怠管理画面に遷移
    webDriver.findElement()で「勤怠」リンク要素を取得後、クリック。
    ロケータはlinkTextを使用。
    JSのalertを操作。
    WebDriverWaitを利用して、待ち時間を設定。
    JSalertが出現したら.accept()でOKボタンを押下。
    画面タイトルを検証後、エビデンスを取得。
    
    test04()
    「出勤」ボタンを押下し出勤時間を登録
    webDriver.findElement()で「出勤」ボタン要素を取得後、クリック。
    ロケータはcssSelectorを利用。
    JSのalertを操作。
    WebDriverWaitを利用して、待ち時間を設定。
    JSalertが出現したら.accept()でOKボタンを押下。
    画面タイトルを検証後、100ピクセルスクロールし、エビデンスを取得。
    
    test05()
    「退勤」ボタンを押下し退勤時間を登録
    webDriver.findElement()で「退勤」ボタン要素を取得後、クリック。
    ロケータはcssSelectorを利用。
    JSのalertを操作。
    WebDriverWaitを利用して、待ち時間を設定。
    JSalertが出現したら.accept()でOKボタンを押下。
    画面タイトルを検証後、100ピクセルスクロールし、エビデンスを取得。

--- test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
+++ test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case10.java
@@ -1,6 +1,11 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +14,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +45,116 @@ public class Case10 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//URLに遷移
+		goTo("http://localhost:8080/lms/");
+
+		//画面タイトル検証
+		pageLoadTimeout(10);
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面操作
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser1234");
+
+		WebElement loginBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']"));
+		loginBtnElement.click();
+
+		//画面タイトル検証
+		visibilityTimeout(By.cssSelector("li.active"), 10);
+		WebElement titleElement = webDriver.findElement(By.cssSelector("li.active"));
+		assertEquals("コース詳細", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//画面操作
+		WebElement attendanceLinkElement = webDriver.findElement(By.linkText("勤怠"));
+		attendanceLinkElement.click();
+
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		alert.accept();
+
+		//画面タイトル検証
+		pageLoadTimeout(10);
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("勤怠管理", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「出勤」ボタンを押下し出勤時間を登録")
 	void test04() {
-		// TODO ここに追加
+		//画面操作
+		WebElement attendanceBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='出勤']"));
+		attendanceBtnElement.click();
+
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		alert.accept();
+
+		//出勤登録検証
+		//現在時刻の用意
+		String now = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+		String attendanceTdXpath = "//tr[contains(@class,'info')]/td[3]";
+		WebElement attendanceTdElement = webDriver.findElement(By.xpath(attendanceTdXpath));
+		assertEquals(now, attendanceTdElement.getText());
+
+		//エビデンス取得
+		scrollBy("100");
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 「退勤」ボタンを押下し退勤時間を登録")
 	void test05() {
-		// TODO ここに追加
+		//画面操作
+		WebElement livingBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='退勤']"));
+		livingBtnElement.click();
+
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(10));
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		alert.accept();
+
+		//退勤登録検証
+		//現在時刻の用意
+		String now = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
+		String livingTdXpath = "//tr[contains(@class,'info')]/td[4]";
+		WebElement livingTdElement = webDriver.findElement(By.xpath(livingTdXpath));
+		assertEquals(now, livingTdElement.getText());
+
+		//エビデンス取得
+		scrollBy("100");
+		getEvidence(new Object() {
+		});
+
 	}
 
 }
