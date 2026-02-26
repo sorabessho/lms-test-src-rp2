620341f59c59134aecbd0340c219dbd073644cde
sorabessho <sora_bessho@asupage.co.jp>
2026-02-26 14:32:07 +0900

    fix:Case11 テストコードの追加
    test01()
    トップページURLでアクセス
    記述内容はCase10.test01()と同様。
    
    test02()
    初回ログイン済みの受講生ユーザーでログイン
    記述内容はCase10.test03()と同様。
    
    test03()
    上部メニューの「勤怠」リンクから勤怠管理画面に遷移
    記述内容はCase10.test03()と同様。
    
    test04()
    「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移
    webElement.findElementを利用し、「勤怠情報を直接編集する」リンクを取得し、クリック。
    画面タイトルを検証後、エビデンスを取得。
    
    test05()
    すべての研修日程の勤怠情報を正しく更新し勤怠管理画面に遷移
    webElement.findElementを利用し、変更予定の全てのプルダウンボックスがはいっている日付の親要素を取得。
    ロケータはxpathを利用。
    for文を使い、一つずつ親要素を取り出し、各プルダウンボックスをidロケータを使用して取得。
    for文のインデックスを活用して、各親ごとにidが変わるよう実装。
    各々、要素取得後取得後、指定の値を選択。
    for文が終了したら、「更新」ボタンを押下。
    JSの警告モーダルが出るので、Util.acceptJSAlert()を使用して処理。
    画面タイトルを検証。
    勤怠情報が更新されたかも検証。
    これも、上記の処理と似た処理を行う。
    親要素を事前に取得し、親ごとに各要素を取得し、assertで検証。
    全て検証し終わり次第、要素までスクロールし、エビデンスを取得。

--- test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
+++ test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f04_attendance/Case11.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f04_attendance;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,9 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * 結合テスト 勤怠管理機能
@@ -35,35 +41,133 @@ public class Case11 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//URLに遷移
+		goTo("http://localhost:8080/lms/");
+
+		//画面タイトル検証
+		pageLoadTimeout(10);
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("ログイン", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		//画面操作
+		WebElement idElement = webDriver.findElement(By.id("loginId"));
+		idElement.clear();
+		idElement.sendKeys("StudentAA01");
+
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("TestUser1234");
+
+		WebElement loginBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='ログイン']"));
+		loginBtnElement.click();
+
+		//画面タイトル検証
+		visibilityTimeout(By.cssSelector("li.active"), 10);
+		WebElement titleElement = webDriver.findElement(By.cssSelector("li.active"));
+		assertEquals("コース詳細", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「勤怠」リンクから勤怠管理画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		//画面操作
+		WebElement attendanceLinkElement = webDriver.findElement(By.linkText("勤怠"));
+		attendanceLinkElement.click();
+
+		acceptJSAlert();
+
+		//画面タイトル検証
+		pageLoadTimeout(10);
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("勤怠管理", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「勤怠情報を直接編集する」リンクから勤怠情報直接変更画面に遷移")
 	void test04() {
-		// TODO ここに追加
+		//画面操作
+		WebElement directEditLinkElement = webDriver.findElement(By.linkText("勤怠情報を直接編集する"));
+		directEditLinkElement.click();
+
+		//画面タイトル検証
+		pageLoadTimeout(10);
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("勤怠管理", titleElement.getText());
+
+		//エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 すべての研修日程の勤怠情報を正しく更新し勤怠管理画面に遷移")
 	void test05() {
-		// TODO ここに追加
+		//画面操作
+		//全てのプルダウンボックスがはいっている日付の親要素を取得
+		String attendanceTrXpath = "//*[@id='main']/div/div/form/table/tbody/tr";
+		List<WebElement> attendanceTrElements = webDriver.findElements(By.xpath(attendanceTrXpath));
+		//各日付の各プルダウンボックスを操作
+		for (int i = 0; i < attendanceTrElements.size(); i++) {
+			WebElement attendanceTrElement = attendanceTrElements.get(i);
+			Select startHourSelect = new Select(attendanceTrElement.findElement(By.id("startHour" + i)));
+			startHourSelect.selectByValue("8");
+
+			Select startMinuteSelect = new Select(attendanceTrElement.findElement(By.id("startMinute" + i)));
+			startMinuteSelect.selectByValue("55");
+
+			Select endHourSelect = new Select(attendanceTrElement.findElement(By.id("endHour" + i)));
+			endHourSelect.selectByValue("18");
+
+			Select endMinuteSelect = new Select(attendanceTrElement.findElement(By.id("endMinute" + i)));
+			endMinuteSelect.selectByValue("15");
+		}
+
+		WebElement updateBtnElement = webDriver.findElement(By.cssSelector("input[type='submit'][value='更新']"));
+		scrollIntoView(updateBtnElement);
+		updateBtnElement.click();
+
+		acceptJSAlert();
+
+		//画面タイトル検証
+		pageLoadTimeout(10);
+		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
+		assertEquals("勤怠管理", titleElement.getText());
+
+		//勤怠情報更新検証
+		String checkAttendanceTrXpath = "//*[@id='main']/div[@class='row']/div/table/tbody/tr";
+		List<WebElement> checkAttendanceTrElements = webDriver.findElements(By.xpath(checkAttendanceTrXpath));
+		for (WebElement checkAttendanceTrElement : checkAttendanceTrElements) {
+			WebElement startElement = checkAttendanceTrElement.findElement(By.xpath("./td[3]"));
+			WebElement endElement = checkAttendanceTrElement.findElement(By.xpath("./td[4]"));
+
+			assertEquals("08:55", startElement.getText());
+			assertEquals("18:15", endElement.getText());
+		}
+
+		//エビデンス取得
+		scrollBy("100");
+		getEvidence(new Object() {
+		});
 	}
 
 }
