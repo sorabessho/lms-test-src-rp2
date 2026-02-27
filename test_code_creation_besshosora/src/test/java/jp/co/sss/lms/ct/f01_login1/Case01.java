cbaf9c8e1018e10ac224a5d2fe53e12f2e471557
sorabessho <sora_bessho@asupage.co.jp>
2026-02-27 14:49:49 +0900

    fix:Case01.java 画面タイトル検証の記述をUtil.checkTitleH2()に置き換え

--- test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_besshosora/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,7 +1,6 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,8 +9,6 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -45,8 +42,7 @@ public class Case01 {
 		goTo("http://localhost:8080/lms/");
 
 		//画面タイトル検証
-		WebElement titleElement = webDriver.findElement(By.tagName("h2"));
-		assertEquals("ログイン", titleElement.getText());
+		checkTitleH2("ログイン");
 
 		//エビデンス取得
 		getEvidence(new Object() {
